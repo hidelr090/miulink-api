acca787a88f39fb3a54c773f212a11a6ebe3f121
hidelr090 <hidelgardo503@gmail.com>
2025-01-11 14:53:50 -0300

    verify identify

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ import urlsRouter from './src/routes/url.js';
 import indexRouter from './src/routes/index.js';
 import connectDB from './src/config/db.js';
 import dotenv from 'dotenv';
+// import cors from 'cors';
 
 dotenv.config();
 
 const PORT = 3333;
 
 connectDB().then(() => console.log('Database Connected')).catch((err) => console.log(err));
+
+// const allowedOrigin = process.env.BASE;
+//
+// const corsOptions = {
+//     origin: allowedOrigin,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+//
+// app.use(cors(corsOptions));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- src/middlewares/indentify.js
+++ src/middlewares/indentify.js
@@ -1,8 +1,7 @@
 import Allowed from "../models/Allowed.js";
 
 export const identify = async (req, res, next) => {
-    const requesterUrl = req.get('origin') || req.get('referer');
-    console.log(requesterUrl);
+    const requesterUrl = req.get('Origin') || req.get('origin');
     const foundRequesterUrl = await Allowed.findOne({url: requesterUrl});
     if (!foundRequesterUrl) {
         return res.status(403).send('Forbidden');
